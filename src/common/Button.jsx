b77495fca70de3d30b91d80a8e588add3f7d0195
Amy Liao <amzhliao@gmail.com>
2024-12-11 16:06:07 -0600

    prop types lib

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import { DONTEXIST, GLOBALTHING, ewstinkie } from 'constants.js';
+import PropTypes from 'prop-types';
 import { TESTTHING } from 'test/constants';
 
 import { THING } from './constants';
+
+Button.propTypes = {
+  children: PropTypes.node,
+};
 
 export default function Button({ children }) {
   console.log('THING', THING);
