1571193c89db89708e3bb08b11ace7b4955e48a3
Amy Liao <amzhliao@gmail.com>
2024-12-14 03:21:30 -0600

    homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,22 +2,38 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Text = styled.p`
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Title = styled.p`
   font-size: 2.5em;
   font-weight: bold;
+  margin: 0;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.5em;
+  margin: 0;
 `;
 
 const HomePage = styled.div`
-  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  text-align: center;
+  padding-top: 100px;
 `;
 
 export default function Home() {
   return (
     <HomePage>
-      <Text>Welcome to the Home Page</Text>
+      <TextContainer>
+        <Title>Welcome to the Home Page</Title>
+        <Subtitle>You are NOT logged in.</Subtitle>
+      </TextContainer>
     </HomePage>
   );
 }
