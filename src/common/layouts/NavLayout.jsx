ae2cab28ac9df80f45e70ae786a64d2bb6ef381e
Amy Liao <amzhliao@gmail.com>
2024-12-17 03:38:57 -0600

    import fix

--- src/common/layouts/NavLayout.jsx
+++ src/common/layouts/NavLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
-import NavBar from 'common/navigation/NavBar';
+import NavBar from 'common/components/navigation/NavBar';
 
 const Layout = styled.div`
   height: 100vh;
