be6b85ba2028bf53ef4e8238f351276808463478
Christian Lee <christianlee101@gmail.com>
2025-04-09 14:22:16 -0500

    Fix imports

--- src/common/components/ui/sidebar.tsx
+++ src/common/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@ import { VariantProps, cva } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 import * as React from "react"
 
-import { useIsMobile } from "@/common/hooks/use-mobile"
+import { useIsMobile } from "@/common/hooks/useMobile"
 import { cn } from "@/lib/utils"
 
 import { Button } from "@/common/components/ui/button"
