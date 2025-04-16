6f3192ca9e0b5d8f95e8e446b46b172e9657ab5c
Christian Lee <christianlee101@gmail.com>
2025-04-16 12:22:41 -0500

    All new components from the template

---
+++ src/common/components/navigation/sidebar/AppSidebar.tsx
@@ -0,0 +1,42 @@
+import { BusinessCard } from '@/common/components/navigation/sidebar/BusinessCard'
+import { NavUser } from '@/common/components/navigation/sidebar/NavUser'
+import { SidebarSection } from '@/common/components/navigation/sidebar/SidebarSection'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail
+} from "@/common/components/ui/sidebar"
+import { sidebarSections } from '@/common/data/sidebar-data'
+
+
+export function AppSidebar({ ...props}: React.ComponentProps<typeof Sidebar>) {
+
+  return (
+    <Sidebar 
+      collapsible='none'
+      variant='floating'
+      {...props}>
+      <SidebarHeader>
+        <BusinessCard />
+      </SidebarHeader>
+      
+      <SidebarContent>
+        {sidebarSections.map((section) => (
+          <SidebarSection
+            key={section.label}
+            label={section.label}
+            items={section.items}
+          />
+        ))}
+      </SidebarContent>
+
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ src/common/components/navigation/sidebar/NavUser.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import {
+  ChevronsUpDown,
+  LogOut,
+  User
+} from "lucide-react"
+
+import { useUser } from '@/common/hooks/useUser'
+import { Link } from 'react-router-dom'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../ui/dropdown-menu"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../../ui/sidebar"
+import UserAvatar from "../../user/UserAvatar"
+
+
+//
+// This is the user profile card at the footer of the sidebar
+//
+export function NavUser() {
+  const { isMobile } = useSidebar()
+  const { user, logout } = useUser();
+
+  if (!user) return null;
+  const name = `${user.firstname ?? ""} ${user.lastname ?? ""}`.trim();
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <UserAvatar profile_picture={user.profile_picture} fullName={name} className="h-8 w-8"/>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <UserAvatar profile_picture={user.profile_picture} fullName={name} className="h-8 w-8"/>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">{name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild>
+                <Link to="my-account">
+                  <User />
+                  Account
+                </Link>
+              </DropdownMenuItem>
+
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={logout}>
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+}
