ddf251cca393817d43806530a01528643a852d2d
Christian Lee <christianlee101@gmail.com>
2025-04-16 13:43:33 -0500

    Sidebar is what I want it to look like

--- src/common/layouts/DashboardLayout.tsx
+++ src/common/layouts/DashboardLayout.tsx
@@ -1,34 +1,34 @@
 import { AppSidebar } from "@/common/components/navigation/sidebar/AppSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/common/components/ui/sidebar";
+import { SidebarProvider } from "@/common/components/ui/sidebar";
+import { SearchProvider } from '@/common/contexts/search-context';
+import { cn } from '@/lib/utils';
+import Cookies from 'js-cookie';
 import { Outlet } from "react-router-dom";
-import styled from 'styled-components';
-
-const LayoutContainer = styled.div`
-  display: flex;
-  min-height: 100vh;
-  width: 100%;
-`;
-
-const MainContent = styled.main`
-  width: 100%;
-  flex: 1;
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-`;
 
 export default function DashboardLayout() {
+  const defaultOpen = Cookies.get('sidebar:state') !== 'false'
+  console.log("default Open:", defaultOpen);
+
   return (
-    <SidebarProvider>
-      <LayoutContainer>
-        <AppSidebar />
-        <SidebarTrigger />
-        <MainContent>
+    <SearchProvider>
+      <SidebarProvider defaultOpen={defaultOpen}>
+          <AppSidebar />
+            <div
+            id='content'
+            className={cn(
+              'ml-auto w-full max-w-full',
+              'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
+              'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
+              'transition-[width] duration-200 ease-in-out',
+              'flex h-svh flex-col',
+              'group-data-[scroll-locked=1]/body:h-full',
+              'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
+            )}
+          >
             <Outlet />
-        </MainContent>
-      </LayoutContainer>
-    </SidebarProvider>
+          </div>
+      </SidebarProvider>
+    </SearchProvider>
+
   );
 }
