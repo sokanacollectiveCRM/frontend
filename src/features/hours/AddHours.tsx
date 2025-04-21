43743182364a6aa9542eb9e5d948519ea07b013d
Keigo Healy <afkhealy@gmail.com>
2025-04-21 14:48:29 -0500

    fixed routes

--- src/features/hours/AddHours.tsx
+++ src/features/hours/AddHours.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/common/components/ui/button";
 import React from "react";
 
 export default function AddHours() {
-  
   return (
     <Button>
       Add Hours
---
+++ src/features/hours/components/users-delete-dialog.tsx
@@ -0,0 +1,86 @@
+'use client'
+import { ConfirmDialog } from '@/common/components/confirm-dialog'
+import { Alert, AlertDescription, AlertTitle } from '@/common/components/ui/alert'
+import { Input } from '@/common/components/ui/input'
+import { Label } from '@/common/components/ui/label'
+import { toast } from '@/common/hooks/use-toast'
+import { TriangleAlert } from 'lucide-react'
+import * as React from 'react'
+import { useState } from 'react'
+import { User } from '../data/schema'
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  currentRow: User
+}
+
+export function UsersDeleteDialog({ open, onOpenChange, currentRow }: Props) {
+  const [value, setValue] = useState('')
+
+  const handleDelete = () => {
+    if (value.trim() !== currentRow.username) return
+
+    onOpenChange(false)
+    toast({
+      title: 'The following user has been deleted:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>
+            {JSON.stringify(currentRow, null, 2)}
+          </code>
+        </pre>
+      ),
+    })
+  }
+
+  return (
+    <ConfirmDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      handleConfirm={handleDelete}
+      disabled={value.trim() !== currentRow.username}
+      title={
+        <span className='text-destructive'>
+          <TriangleAlert
+            className='mr-1 inline-block stroke-destructive'
+            size={18}
+          />{' '}
+          Delete User
+        </span>
+      }
+      desc={
+        <div className='space-y-4'>
+          <p className='mb-2'>
+            Are you sure you want to delete{' '}
+            <span className='font-bold'>{currentRow.username}</span>?
+            <br />
+            This action will permanently remove the user with the role of{' '}
+            <span className='font-bold'>
+              {currentRow.role.toUpperCase()}
+            </span>{' '}
+            from the system. This cannot be undone.
+          </p>
+
+          <Label className='my-2'>
+            Username:
+            <Input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder='Enter username to confirm deletion.'
+            />
+          </Label>
+
+          <Alert variant='destructive'>
+            <AlertTitle>Warning!</AlertTitle>
+            <AlertDescription>
+              Please be carefull, this operation can not be rolled back.
+            </AlertDescription>
+          </Alert>
+        </div>
+      }
+      confirmText='Delete'
+      destructive
+    />
+  )
+}
