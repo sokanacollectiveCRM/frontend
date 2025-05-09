ac59c4f1e309100634b46a095b4932492ad5d1d1
Daniel Antony <danantony06@gmail.com>
2025-05-08 20:40:36 -0500

    Export Demographics End to End

--- src/common/components/ui/dialog.tsx
+++ src/common/components/ui/dialog.tsx
@@ -92,6 +92,20 @@ function DialogDescription({
   )
 }
 
+const DialogFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className
+    )}
+    {...props}
+  />
+)
+DialogFooter.displayName = 'DialogFooter'
+
 export {
   Dialog,
   DialogClose,
--- src/features/clients/components/users-primary-buttons.tsx
+++ src/features/clients/components/users-primary-buttons.tsx
@@ -17,6 +17,31 @@ import { DraggableTemplate, mockTemplates } from './TemplateSidebar'
 export function UsersPrimaryButtons() {
   const templates = mockTemplates; // assume it returns a list of { id, title }
 
+  const fetchCSV = async() => {
+    const token = localStorage.getItem('authToken');
+    try {
+      const data = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}clients/fetchCSV`,{
+        credentials: 'include',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      const csvData = await data.text();
+      const blob = new Blob([csvData], { type: "text/csv" });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "demographics.csv"; 
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+
+    } catch (error) {
+      throw new Error(`Error Retrieving CSV${error}`)
+    }
+  }
   return (
     <div className="flex gap-2">
       <Popover>
@@ -40,6 +65,10 @@ export function UsersPrimaryButtons() {
           </Command>
         </PopoverContent>
       </Popover>
+      <Button className="space-x-1" onClick={fetchCSV}>
+            <span>Export Demographics</span>
+            <SquarePlus size={18} />
+      </Button>
     </div>
   )
 }
