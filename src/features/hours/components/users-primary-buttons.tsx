6447d04ce05eb67ad719dcf8d0d9bb887bf2baf5
Keigo Healy <afkhealy@gmail.com>
2025-05-10 15:55:17 -0500

    completed frontend

--- src/features/hours/components/users-primary-buttons.tsx
+++ src/features/hours/components/users-primary-buttons.tsx
@@ -12,61 +12,66 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/common/components/ui/sheet"
-import ChooseDate from './add-date-dialog'
+import { DateTimePicker } from './time-date-pick'
 
 export function UsersPrimaryButtons() {
-  const [startDate, setStartDate] = React.useState<Date | undefined>(new Date());
-  const [startTime, setStartTime] = useState<string | undefined>(undefined);
+  // const [startDate, setStartDate] = React.useState<Date | undefined>(new Date());
+  // const [startTime, setStartTime] = useState<string | undefined>(undefined);
+  // const [endDate, setEndDate] = React.useState<Date | undefined>(new Date());
+  // const [endTime, setEndTime] = useState<string | undefined>(undefined);
+  
+  const [startDate, setStartDate] = React.useState<Date>();
   const [endDate, setEndDate] = React.useState<Date | undefined>(new Date());
-  const [endTime, setEndTime] = useState<string | undefined>(undefined);
 
-  const printUserStuff = () => {
-    console.log("ADD HOURS BUTTON CLICKED");
+  function handleSave() {
+    console.log("start time", startDate); 
+    console.log("end time", endDate);
+
+    
   }
 
   return (
     <div className='flex gap-2'>
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button className='space-x-1' onClick={printUserStuff}>
-          <span>Add Hours</span> <SquarePlus size={18} />
-        </Button>
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle className='text-2xl font-bold'>Add Hours</SheetTitle>
-          <SheetDescription>
-            Please enter your start and end time 
-          </SheetDescription>
-        </SheetHeader>
-        <div className="flex flex-col gap-10 justify-center items-center mt-10">
-          <div className="items-center flex items-center justify-center">
-            <ChooseDate 
-              trigger_text="Select start date"
-              dialog_title="When did you START work?"
-              date={startDate}
-              setDate={setStartDate}
-            />
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className='space-x-1'>
+            <span>Add Hours</span> <SquarePlus size={18} />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className='text-2xl font-bold'>Add Hours</SheetTitle>
+            <SheetDescription>
+              Please enter your start and end time 
+            </SheetDescription>
+          </SheetHeader>
+          <div className="flex flex-col gap-10 justify-center items-center mt-10">
+            <div className="items-center flex items-center justify-center">
+              <div className='flex flex-col gap-10'>
+                <div>
+                  <h1 className='text-lg'>When did you <span className='font-bold'>start</span> work?</h1>
+                  <DateTimePicker 
+                    date={startDate}
+                    setDate={setStartDate}
+                  />
+                </div>
+                <div>
+                  <h1 className='text-lg'>When did you <span className='font-bold'>end</span> work?</h1>
+                  <DateTimePicker 
+                    date={endDate}
+                    setDate={setEndDate}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="items-center flex items-center justify-center">
-          </div>
-          <div className="items-center flex items-center justify-center">
-            <ChooseDate 
-              trigger_text="Select end date"
-              dialog_title="When did you END work?"
-              date={endDate}
-              setDate={setEndDate}
-            />
-
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit" onClick={() => {console.log("start time", startDate); console.log("end time", endDate);}}>Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit" onClick={handleSave}>Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
